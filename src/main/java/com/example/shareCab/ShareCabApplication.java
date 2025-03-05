63ce0e4089cf2c8f78a168ea1b2eeef7e051b7af
voidAasif <aasifsaifi9280@gmail.com>
2025-03-05 21:46:26 +0530

    Initial Commit

---
+++ src/main/java/com/example/shareCab/ShareCabApplication.java
@@ -0,0 +1,13 @@
+package com.example.shareCab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareCabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shareCab/ShareCabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareCab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareCabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
